4444f665a43d74df6197bddbd07620ea4ac9c01e
Eli Kim <me@eunchan.kim>
2024-10-17 23:16:28 -0700

    Increase font and custom extra-sass plugin

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # Custom git revision date localized plugin
 pip3 install git+https://github.com/eunchan/mkdocs-git-revision-date-localized-plugin.git#egg=mkdocs-git-revision-date-localized-plugin
+pip3 install git+https://github.com/eunchan/mkdocs-extra-sass-plugin.git#egg=mkdocs-extra-sass-plugin
 pip3 install livereload mkdocs-extra-sass-plugin
 pip3 install "mkdocs-material[imaging]"
 pip3 install python-frontmatter
