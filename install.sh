ce3d9628af97ff4f38e30a4a86aacc2f9867ce4c
Eli Kim <me@eunchan.kim>
2024-10-19 11:20:37 -0700

    Remove official mkdocs-extra-sass-plugin install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Custom git revision date localized plugin
 pip3 install git+https://github.com/eunchan/mkdocs-git-revision-date-localized-plugin.git#egg=mkdocs-git-revision-date-localized-plugin
 pip3 install git+https://github.com/eunchan/mkdocs-extra-sass-plugin.git#egg=mkdocs-extra-sass-plugin
-pip3 install livereload mkdocs-extra-sass-plugin
+pip3 install livereload
 pip3 install "mkdocs-material[imaging]"
 pip3 install python-frontmatter
 brew install cairo freetype libffi libjpeg libpng zlib
